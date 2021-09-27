3bb5cac067b3c995ad620f1d56bf17d75997c64e
Adrian <grimm3960@gmail.com>
2021-09-27 18:52:31 +0300

    "change Controller"

--- my-taxi-service/core/src/main/java/ru/digitalleague/core/controller/Controller.java
+++ my-taxi-service/core/src/main/java/ru/digitalleague/core/controller/Controller.java
@@ -3,6 +3,7 @@ package ru.digitalleague.core.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,11 @@ import ru.digitalleague.core.model.OrderDetails;
 @RestController
 public class Controller {
 
-    private final RabbitTemplate template;
-    private final ObjectMapper mapper ;
+    @Autowired
+    private ObjectMapper mapper;
 
-    public Controller(ObjectMapper mapper, RabbitTemplate template) {
-        this.mapper = mapper;
-        this.template = template;
-    }
+    @Autowired
+    private RabbitTemplate template;
 
     @PostMapping("/order")
     public void addNewOrderDetails(@RequestBody OrderDetails orderDetails) {
